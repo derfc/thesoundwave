89214a3242a2ff4121556edc1dc7099f4781bb5e
Chan Tsz Him Oscar <Oscar@wailuntiiPhone2.localdomain>
2021-03-15 11:55:04 +0800

    hello

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,3 +1,4 @@
+const { default: knex } = require('knex')
 const passport = require('passport')
 const googleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
@@ -5,16 +6,33 @@ const keys = require('./keys')
 //clientID: 694064478458-c0n5152oi73nb9cd9erlemqrdour867o.apps.googleusercontent.com
 //client secret: f5-jq-TOv_OLnnqFu9DURoiF
 
+
+
 passport.use(
     new googleStrategy({
         callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
 
-    }, (accessToken, refreshToken, profile, done) => {
+    }, async (accessToken, refreshToken, profile, done) => {
         // passport callback
         console.log('passport callback fired')
         console.log(profile)
+        const newuser = { username: profile.displayName, googleID: profile.id }
+
+        // insert database
+
+        const user = await knex('users').where('google_id', user.googleID)
+        console.log(user)
+
+
+        knex('users').insert({ username: newuser.username, google_id: newuser.googleID })
+            .then(() => {
+                console.log('user inserted')
+            })
+
+
+
     })
 )
 
