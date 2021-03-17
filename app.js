2760df029fc58c78644566e5c8b8e4d34fcdcf0b
derfc <fredcheung@live.hk>
2021-03-17 16:14:38 +0800

    payment, del cart item

--- app.js
+++ app.js
@@ -6,27 +6,32 @@ let storeSQL = new StoreSQL("users", "store", "item", "cart");
 
 const app = express();
 const port = 3000;
-const authRoutes = require('./routes/auth-routes')
-const passportSetup = require('./config/passport-setup')
-const keys = require('./config/keys')
-const passport = require('passport')
+const authRoutes = require("./routes/auth-routes");
+const passportSetup = require("./config/passport-setup");
+const keys = require("./config/keys");
+const passport = require("passport");
+
+const stripePublicKey = process.env.stripe_pk;
+const stripeSecretKey = process.env.stripe_sk;
+const stripe = require("stripe")(stripeSecretKey);
 
 //cookie session
-const cookieSession = require('cookie-session')
+const cookieSession = require("cookie-session");
 
-app.use(cookieSession({
-	//1 day
-	maxAge: 24 * 60 * 60 * 1000,
-	keys: [keys.session.cookieKey]
-}))
-
+app.use(
+	cookieSession({
+		//1 day
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: [keys.session.cookieKey],
+	})
+);
 
 //initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 //auth routes
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
 
 //handlebars
 app.engine(
@@ -49,7 +54,7 @@ app.engine(
 app.set("view engine", "handlebars");
 
 //body-parser
-app.use(bodyParser.urlencoded({ extended: true, }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //static files
@@ -60,18 +65,22 @@ app.use(express.static(__dirname + "/public"));
 //authcheck
 const authCheck = (req, res, next) => {
 	if (!req.user) {
-		res.redirect('/auth/login');
+		res.redirect("/auth/login");
 	} else {
 		next();
 	}
-}
+};
+
+// const hello = () => {
+// 	console.log(stripeSecretKey);
+// };
+// hello();
 
 //index route
 //2 btn to login/register
 app.get("/", (req, res) => {
 	res.render("index");
 });
-
 
 //login route
 // app.get("/login", (req, res) => {
@@ -97,13 +106,16 @@ app.post("/register", (req, res) => {
 
 //home route
 app.get("/home", authCheck, (req, res) => {
-	res.render("home", { layout: "dashboard", user: req.user.displayName, thumbnail: req.user._json.picture });
+	res.render("home", {
+		layout: "dashboard",
+		user: req.user.displayName,
+		thumbnail: req.user._json.picture,
+	});
 
 	// console.log(req.user.displayName)
 	// res.send('you are logged in ' + req.user.displayName)
 	//render user name, playlist, lots of btns, browsing
 });
-
 
 //setting route
 app.get("/setting", (req, res) => {
@@ -119,7 +131,11 @@ app.put("/setting", (req, res) => {
 //store route
 app.get("/store", (req, res) => {
 	return storeSQL.getStoreItem().then((item) => {
-		res.render("store", { item: item, layout: "dashboard" });
+		res.render("store", {
+			item: item,
+			layout: "dashboard",
+			stripePublicKey: stripePublicKey,
+		});
 	});
 });
 
@@ -127,22 +143,66 @@ app.get("/store", (req, res) => {
 app.get("/cart", (req, res) => {
 	let user_id = 1;
 	return storeSQL.getCartItem(user_id).then((item) => {
-		console.log("hello", item);
-		res.render("cart", { item: item, layout: "dashboard" });
+		res.render("cart", {
+			item: item,
+			layout: "dashboard",
+			stripePublicKey: stripePublicKey,
+		});
 	});
 });
 
 app.post("/cart", (req, res) => {
 	let item_id = req.body.item_id;
 	let user_id = req.body.user_id;
-	console.log(item_id, user_id);
 	return storeSQL.addToCart(user_id, item_id).then(() => {
-		res.render("cart", { layout: "dashboard" });
+		res.render("cart", {
+			layout: "dashboard",
+			stripePublicKey: stripePublicKey,
+		});
 	});
 });
 
-app.delete("/cart/:id", (req, res) => {
-	// res.render("cart", { layout: "dashboard" });
+app.post("/purchase", (req, res) => {
+	let total = 0;
+	let count = 0;
+	req.body.items.forEach((item) => {
+		storeSQL.getItemPrice(item.item_id).then((data) => {
+			total += data[0].item_price * item.quantity;
+			count++;
+			if (count === req.body.items.length) {
+				// charge below
+				// stripe.charges
+				// 	.create({
+				// 		amount: total,
+				// 		source: req.body.stripeTokenId,
+				// 		currency: "usd",
+				// 	})
+				// 	.then(() => {
+				// 		console.log("charge successful");
+				// //clear cart
+				// 		res.json({ message: "successfully purchased" });
+				// 	})
+				// 	.catch((err) => {
+				// 		console.log("charge fail", err);
+				// 		res.status(500).end();
+				// 	});
+			}
+		});
+	});
+});
+
+app.delete("/cart/:user_id/:item_id", (req, res) => {
+	let user_id = req.params.user_id;
+	let delete_item = req.params.item_id;
+	return storeSQL.delCartItem(user_id, delete_item).then(() => {
+		return storeSQL.getCartItem(user_id).then((item) => {
+			res.render("cart", {
+				item: item,
+				layout: "dashboard",
+				stripePublicKey: stripePublicKey,
+			});
+		});
+	});
 });
 
 app.listen(port, () => {
