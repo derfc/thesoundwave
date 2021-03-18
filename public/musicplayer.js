1fa74f2f35fd5cd8ac72fed73bce613c870f289a
Chan Tsz Him Oscar <Oscar@wailuntiiPhone2.localdomain>
2021-03-18 16:55:35 +0800

    musicbar done

--- public/musicplayer.js
+++ public/musicplayer.js
@@ -5,7 +5,6 @@ let next = $('#next')
 let volume = $('#volume');
 let displayVolume = $('#volume_show');
 let slider = $('#duration_slider');
-let show_duration = $('#show_duration');
 let track = $('#myTrack')
 let current_time = $('#current_time')
 // let track_image = $('#track_image');
@@ -15,30 +14,14 @@ let current_time = $('#current_time')
 
 let Playing_song = false;
 
-// // function update() {
-// //     console.log(track[0].duration)
-// //     setTimeout(function () {
-// //         console.log("sto", track[0].duration)
-// //         var minutes = pad(parseInt(track[0].duration / 60));
-// //         var seconds = pad(parseInt(track[0].duration % 60));
-// //         show_duration[0].innerHTML = `<span>${minutes + ':' + seconds}</span>`
-// //     }, 500)
-
-// // }
-// // function pad(d) {
-// //     return (d < 10) ? '0' + d.toString() : d.toString();
-// // }
-// update()
-
 
 function updateTrackTime() {
-    var currTimeDiv = $('#current_time');
-    var durationDiv = $('#show_duration');
-
-    var currTime = Math.floor(track[0].currentTime).toString();
-    var duration = Math.floor(track[0].duration).toString();
-
+    let currTimeDiv = $('#current_time');
+    let durationDiv = $('#show_duration');
+    let currTime = Math.floor(track[0].currentTime).toString();
+    let duration = Math.floor(track[0].duration).toString();
     currTimeDiv[0].innerHTML = formatSecondsAsTime(currTime);
+    slider[0].value = Math.floor(track[0].currentTime / track[0].duration * 100).toString()
 
     if (isNaN(duration)) {
         durationDiv[0].innerHTML = '00:00';
@@ -49,9 +32,9 @@ function updateTrackTime() {
 }
 
 function formatSecondsAsTime(secs, format) {
-    var hr = Math.floor(secs / 3600);
-    var min = Math.floor((secs - (hr * 3600)) / 60);
-    var sec = Math.floor(secs - (hr * 3600) - (min * 60));
+    let hr = Math.floor(secs / 3600);
+    let min = Math.floor((secs - (hr * 3600)) / 60);
+    let sec = Math.floor(secs - (hr * 3600) - (min * 60));
     if (min < 10) {
         min = "0" + min;
     }
@@ -77,8 +60,6 @@ function playsong() {
     Playing_song = true;
     play[0].innerHTML = "<span class='fa-stack'><i class='fas fa-circle fa-stack-2x text-warning'></i><i class='fa fa-pause fa-stack -1x' aria-hidden='true'></i></span>";
     console.log(track[0].currentTime)
-
-
 }
 
 function pausesong() {
@@ -100,21 +81,3 @@ function volume_change() {
     track[0].volume = volume[0].value / 100;
 }
 
-function range_slider() {
-    let position = 0;
-    // update slider position
-    if (!isNaN(track[0].duration)) {
-        position = track[0].currentTime * (100 / track[0].duration);
-        slider[0].value = position;
-    }
-
-    // function will run when the song is over
-    if (track[0].ended) {
-        play[0].innerHTML = "<span class='fa-stack'><i class='fas fa-circle fa-stack-2x text-warning'></i><i class='fa fa-play fa-stack -1x' aria-hidden='true'></i></span>";
-        // if (autoplay == 1) {
-        //     index_no += 1;
-        //     load_track(index_no);
-        //     playsong();
-        // }
-    }
-}
