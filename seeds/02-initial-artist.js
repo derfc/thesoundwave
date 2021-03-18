21fb5a7618f38615821dbb85731e16fbde4968a3
derfc <fredcheung@live.hk>
2021-03-18 14:01:41 +0800

    seeds 02 05 08

--- seeds/02-initial-artist.js
+++ seeds/02-initial-artist.js
@@ -8,6 +8,16 @@ exports.seed = function (knex) {
 				{ id: 1, artist_name: "afrojack", artist_photo: "afrojack.jpg" },
 				{ id: 2, artist_name: "troyboi", artist_photo: "troyboi.jpg" },
 				{ id: 3, artist_name: "eason chan", artist_photo: "eason_chan.jpg" },
+				{
+					id: 4,
+					artist_name: "jacky cheung",
+					artist_photo: "jacky_cheung.jpg",
+				},
+				{
+					id: 5,
+					artist_name: "my little airport",
+					artist_photo: "mla.jpg",
+				},
 			]);
 		});
 };
