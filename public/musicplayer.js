815f95488f39fde7ad4f84e7a07f137a63587ebd
Chan Tsz Him Oscar <Oscar@wailuntiiPhone2.localdomain>
2021-03-19 17:17:39 +0800

    all functions done

--- public/musicplayer.js
+++ public/musicplayer.js
@@ -17,6 +17,7 @@ let current_time = $("#current_time");
 let Playing_song = false;
 let index = 0
 let random = 0;
+let loop = 0;
 
 let songList = [];
 let songNameArr = songName.split("/");
@@ -24,13 +25,27 @@ let songUrlArr = songUrl.split("[split]");
 let artistNameArr = artistName.split("/");
 
 for (let i = 0; i < songNameArr.length - 1; i++) {
-	songList.push({
-		song_name: songNameArr[i],
-		song_url: songUrlArr[i],
-		artist_name: artistNameArr[i],
-	});
+    songList.push({
+        song_name: songNameArr[i],
+        song_url: songUrlArr[i],
+        artist_name: artistNameArr[i],
+    });
 }
 let playlist = JSON.parse(JSON.stringify(songList));
+
+function repeat_song() {
+    loop++
+    console.log(loop)
+    if (loop % 2) {
+        $('#repeating').removeClass().addClass('fas fa-repeat-1-alt')
+        track[0].loop = true;
+        console.log('repeat 1')
+    } else {
+        $('#repeating').removeClass().addClass('fas fa-repeat-alt')
+        track[0].loop = false;
+        console.log('no repeat')
+    }
+}
 
 function random_song() {
     random++
@@ -41,7 +56,9 @@ function random_song() {
     } else {
         $('#random').removeClass();
         console.log(random, 'not random')
-        load_playlist()
+        index = songList.findIndex(x => x.song_name == playlist[index].song_name)
+        // load_playlist()
+        playsong();
     }
 }
 
@@ -59,7 +76,10 @@ load_playlist()
 
 // next song
 function next_song() {
+    loop = 0;
+    track[0].loop = false;
     if (index < songList.length - 1) {
+        $('#repeating').removeClass().addClass('fas fa-repeat-alt')
         index += 1;
         load_playlist();
         reset_slider();
@@ -74,6 +94,9 @@ function next_song() {
 
 // previous song
 function previous_song() {
+    loop = 0;
+    track[0].loop = false;
+    $('#repeating').removeClass().addClass('fas fa-repeat-alt')
     if (index > 0) {
         index -= 1;
         load_playlist();
@@ -130,9 +153,12 @@ function pausesong() {
     console.log("paused");
     play[0].innerHTML =
         "<span class='fa-stack'><i class='fas fa-circle fa-stack-2x text-warning'></i><i class='fa fa-play fa-stack -1x' aria-hidden='true'></i></span>";
+    console.log(loop)
 }
 
 function audio_end() {
+
+
     index++;
     load_playlist();
     reset_slider();
